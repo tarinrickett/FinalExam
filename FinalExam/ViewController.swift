e2be55fe4fe8645870394d6d5961150fb4979e2c
tarinrickett <trickett96@gmail.com>
2016-12-22 11:26:54 -0500

    Initial Creation

---
+++ FinalExam/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FinalExam
+//
+//  Created by Tarin Rickett on 12/22/16.
+//  Copyright © 2016 Mobile Application Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
